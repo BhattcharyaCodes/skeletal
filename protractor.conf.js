d5e37706d44fe4d426a788cebac9f948a655a551
BhattacharyaCodes <puja.bhattacharya.pb@gmail.com>
2019-11-13 20:50:08 +0530

    2nd commit

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,9 +1,12 @@
-export.config = {
-    seleniumAddress: 'https://localhost:4444/wd/hub',
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     capabilities: {
         'browserName': 'chrome'
     },
     framework: 'jasmine',
-    specs: ['specs.js'],
-
+    specs: ['/e2e_test_suite/specs.js'],
+    // Options to be passed to Jasmine.
+     jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+     }
 };
